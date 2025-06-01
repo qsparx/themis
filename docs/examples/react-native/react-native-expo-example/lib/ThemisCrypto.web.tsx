68a31b48c33e7a1198020e6909c7e8f44fef4c8e
QSparx Developer <admin@qsparx.com>
2025-06-01 18:16:20 -0400

    Added wasm support for web

--- docs/examples/react-native/react-native-expo-example/lib/ThemisCrypto.web.tsx
+++ docs/examples/react-native/react-native-expo-example/lib/ThemisCrypto.web.tsx
@@ -1,18 +1,69 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Buffer } from 'buffer';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
+import { Asset } from 'expo-asset';
+import themis, { initialize } from 'wasm-themis';
+
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 export default function ThemisCrypto() {
 	
+	useEffect(() => {
+        async function initThemis() {
+            try {
+                console.log("Initializing Themis wasm...");
+                // Load the WASM file as an asset
+                const asset = Asset.fromModule(require('@/public/assets/wasm/libthemis.wasm'));
+                await asset.downloadAsync();
+                
+                // Initialize Themis with the asset URI
+                console.log("Initializing Themis with asset URI:", asset.uri);
+                await initialize(asset.uri);
+                console.log("Themis initialized");
 
+                // You can now use Themis functions
+                let message = 'Test Message Please Ignore'
+                let context = 'Secure Cell example code'
+                let master_key = Buffer.from('bm8sIHRoaXMgaXMgbm90IGEgdmFsaWQgbWFzdGVyIGtleQ==', 'base64')
+                let passphrase = 'My Litte Secret: Passphrase Is Magic'
+
+                let encrypted_message, decrypted_message
+
+                console.log('# Secure Cell in Seal mode\n')
+
+                console.log('## Master key API\n')
+
+                let scellMK = themis.SecureCellSeal.withKey(master_key)
+
+                encrypted_message = scellMK.encrypt(Buffer.from(message, 'UTF-8'))
+                console.log('Encrypted: ' + Buffer.from(encrypted_message).toString('base64'))
+
+                decrypted_message = scellMK.decrypt(encrypted_message)
+                console.log('Decrypted: ' + Buffer.from(decrypted_message).toString('UTF-8'))
+
+                // Visit https://docs.cossacklabs.com/simulator/data-cell/
+                console.log()
+                encrypted_message = Buffer.from('AAEBQAwAAAAQAAAAEQAAAC0fCd2mOIxlDUORXz8+qCKuHCXcDii4bMF8OjOCOqsKEdV4+Ga2xTHPMupFvg==', 'base64')
+                decrypted_message = scellMK.decrypt(encrypted_message)
+                console.log('Decrypted (simulator): ' + Buffer.from(decrypted_message).toString('UTF-8'))
+
+
+            } catch (err) {
+                console.error("Exception: Failed to initialize Themis wasm", err);
+            }
+        }
+        
+        initThemis();
+	 }, []);
+		
   return (
   <ThemedView style={styles.stepContainer}>
     <ThemedText type="subtitle">Exploring Themis on Web</ThemedText>
     <ThemedText>
-      {`Keep your data safe.`}
+      {`Keep your data safe...`}
     </ThemedText>
   </ThemedView>
   );
